02be29adcba418449c59fa346d3e299e90f80ba9
Remi Marchand <remievan@hotmail.com>
2020-07-11 14:14:20 -0400

    Move the navbar into its own component

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,30 +2,8 @@
 	export let segment;
 </script>
 
+<!--suppress CssUnusedSymbol -->
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
 	.selected {
 		position: relative;
 		display: inline-block;
@@ -34,27 +12,25 @@
 	.selected::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
+		width: calc(100%);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: lightblue;
 		display: block;
 		bottom: -1px;
 	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
 </style>
 
-<nav>
-	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+<nav class="flex items-center justify-between flex-wrap text-white bg-orange-500 p-5">
+    <span class="font-semibold text-xl tracking-tight mr-6">
+		    <a class='{segment === undefined ? "selected" : ""}' href=".">Remi Marchand</a>
+    </span>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
-	</ul>
+    <ul class="block flex-grow font-normal">
+		    <li class="float-left ml-5"><a href="https://resume.remimstr.com">Resume</a></li>
+		    <li class="float-left ml-5"><a href="https://github.com/remimstr">Github</a></li>
+    </ul>
+
+    <button class="m">
+        Contact
+    </button>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,18 +5,6 @@
 </script>
 
 <main>
-    <nav class="flex items-center justify-between flex-wrap text-white bg-orange-500 p-5">
-        <span class="font-semibold text-xl text-white tracking-tight mr-6">Remi Marchand</span>
-
-        <div class="block flex-grow">
-            <span class="ml-4">Resume</span>
-            <span class="ml-4">Github</span>
-        </div>
-
-        <button class="m">
-            Contact
-        </button>
-    </nav>
     <div class="px-4 py-4 text-center">
 		    <Typewriter interval={65}>
             <h1 class="text-2xl sm:text-4xl md:text-5xl">{@html `&lta&gtFront End Development&lt/a&gt`}</h1>
