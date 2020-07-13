84c72e737a44819d4d305f426aeb6ef826f9203f
Remi Marchand <remievan@hotmail.com>
2020-07-13 07:38:48 -0400

    Add about me section (previously forgotten)

---
+++ src/routes/about-me.svelte
@@ -0,0 +1,23 @@
+<main class="flex p-5 justify-center">
+		<div class="max-w-4xl">
+        <h2>
+            My name is Remi Marchand, I am a web developer from Canada
+        </h2>
+        <h3>
+            I have experience in:
+        </h3>
+        <ul>
+            <li>Front-end Development (various JavaScript technologies)</li>
+            <li>Product Design, Development, and Planning</li>
+            <li>Building Enterprise Software</li>
+            <li>Some Devops Activities</li>
+        </ul>
+
+        <h3>
+            Contact me at:
+        </h3>
+        <ul>
+            Email: remi@remimstr.com
+        </ul>
+		</div>
+</main>
