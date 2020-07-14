668eda44e23b03a0c97998ffd089cc4471db35e2
Remi Marchand <remievan@hotmail.com>
2020-07-14 09:29:17 -0400

    Make the navbar responsive

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,7 @@
 <script>
 	export let segment;
+
+	let menuExpanded = false;
 </script>
 
 <!--suppress CssUnusedSymbol -->
@@ -21,16 +23,25 @@
 </style>
 
 <nav class="flex items-center justify-between flex-wrap text-white bg-orange-500 p-3">
-    <span class="font-semibold text-xl tracking-tight">
+    <span class="font-semibold text-xl tracking-tight mr-8">
 		    <a class='text-white {segment === undefined ? "selected" : ""}' href=".">Remi Marchand</a>
     </span>
 
-    <ul class="block flex-grow font-normal list-none">
-		    <li class="float-left ml-5"><a class="text-white" href="https://resume.remimstr.com">Resume</a></li>
-		    <li class="float-left ml-5"><a class="text-white" href="https://github.com/remimstr">Github</a></li>
-    </ul>
+		<div class="block md:hidden" on:click={() => menuExpanded = !menuExpanded}>
+				<button class="flex items-center px-3 py-2 border rounded hover:border-primary hover:text-primary">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+				</button>
+		</div>
 
-    <span class="tracking-tight">
-		    <a class='text-white {segment === "about-me" ? "selected" : ""}' href="./about-me">About Me</a>
-    </span>
+		<div class="w-full block flex-grow md:flex md:items-center md:w-auto" class:hidden={!menuExpanded}>
+					<ul class="block flex-grow font-normal list-none ml-0">
+							<li class="block mt-4 md:inline-block md:mt-0 mr-4"><a class="text-white" href="https://resume.remimstr.com">Resume</a></li>
+							<li class="block mt-4 md:inline-block md:mt-0 mr-4"><a class="text-white" href="https://github.com/remimstr">Github</a></li>
+					</ul>
+
+        <div>
+		      <a class='block mt-4 md:inline-block md:mt-0 mr-4 text-white {segment === "about-me" ? "selected" : ""}' href="./about-me">About Me</a>
+        </div>
+		</div>
+
 </nav>
