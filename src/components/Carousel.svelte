d1fc8a52b28caa92b46a673b6a9e9904eb57a666
Remi Marchand <remievan@hotmail.com>
2020-07-15 21:40:30 -0400

    Swap out tippy implementation for something more custom

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,51 +1,62 @@
 <script>
 		import Carousel from "@beyonk/svelte-carousel";
-		import tippy from "sveltejs-tippy";
+		import _ from "lodash";
+		import tippy from "tippy.js";
+		import "tippy.js/dist/tippy.css";
 
 		const logos = [
 				{
             image: "logos/react.png",
 						name: "React",
+						id: "react",
 						description: "React is my goto web development library. I've used it at Acerta for over 2 years"
 				},
 				{
             image: "logos/typescript.png",
 						name: "TypeScript",
+						id: "typescript",
 						description: "Since converting Acerta's front-end to TypeScript, I can't imagine going back. I LOVE type safety, code completion, and more!"
 				},
 				{
             image: "logos/echarts.png",
 						name: "Echarts",
+						id: "echarts",
 						description: "Echarts is an extremely powerful charting library with a very well documented object-based API."
 				},
 				{
             image: "logos/docker.png",
 						name: "Docker",
+						id: "docker",
 						description: "The tool that needs no introduction, I've wrapped a number of webapps into Docker containers for deployment."
 				},
 				{
             image: "logos/moqups.png",
 						name: "Moqups",
+						id: "moqups",
 						description: "Before designing website mocks in Moqups, I wrote them out by hand. This tool is a fantastic way to convey ideas."
 				},
 				{
             image: "logos/styled-components.png",
 						name: "Styled Components",
+						id: "styled-components",
 						description: "One of the best ways to deal with CSS in a big front-end project, I love how Styled Components makes JS/CS interop so easy!"
 				},
 				{
             image: "logos/tailwind-css.svg",
 						name: "Tailwind CSS",
+						id: "tailwind-css",
 						description: "When building a custom UI from scratch, I now reach for tailwind. Despite a learning curve that looks daunting, it's not that hard."
 				},
 				{
             image: "logos/svelte.png",
 						name: "Svelte",
+						id: "svelte",
 						description: "Being that it's the new kid on the block, I haven't spent a lot of time with Svelte, but building this website with it was a joy!"
 				},
         {
             image: "logos/antd.jpeg",
             name: "Antd",
+						id: "antd",
             description: "The heavy-lifting enterprise library for UI components that doesn't look google-y. Would recommend."
         },
 				{
@@ -56,22 +67,27 @@
 		]
 
     function makeTippyProps(content) {
-		  return {
-				content: `<div class="tippy-tooltip">${content}</div>`,
-				allowHTML: true,
-				placement: "bottom"
-      }
+				return {
+						content: `<div class="tippy-tooltip">${content}</div>`,
+						allowHTML: true,
+						placement: "bottom"
+				}
     };
 
     let carousel;
 
 		function enter() {
-		  carousel.pause();
+				carousel.pause();
 		}
 
 		function leave() {
-		  carousel.resume();
+				carousel.resume();
 		}
+
+		import { onMount } from "svelte";
+		onMount(() => {
+		    logos.forEach(({ id, description }) => tippy(`.${id}`, makeTippyProps(description)));
+		});
 
 		export let hasBeenVisited;
 </script>
@@ -98,12 +114,12 @@
     <h2 class="accordion-section m-2">My Favourite Tech</h2>
     <hr class="w-full block border border-blue-400 line"/>
 		<div class="w-full lg:w-3/5 md:w-4/5">
-			<Carousel perPage={{ 800: 6, 500: 3 }} easing="ease-in-out" autoplay=2000 dots={false} controls={false} bind:this={carousel}>
-				{#each logos as logo (logo)}
-					<div class="flex flex-col slide-content cursor-pointer" on:mouseenter={enter} on:mouseleave={leave}>
-						<img class="h-12 object-scale-down m-3" src={logo.image} alt="{logo.name} logo" use:tippy={makeTippyProps(logo.description)} />
-					</div>
-				{/each}
-			</Carousel>
+				<Carousel perPage={{ 800: 6, 500: 3 }} easing="ease-in-out" autoplay=2000 dots={false} controls={false} bind:this={carousel}>
+						{#each logos as logo (logo)}
+								<div class="flex flex-col slide-content cursor-pointer {logo.id}" on:mouseenter={enter} on:mouseleave={leave}>
+										<img class="h-12 object-scale-down m-3" src={logo.image} alt="{logo.name} logo" />
+								</div>
+						{/each}
+				</Carousel>
 		</div>
 </div>
