40ff328918bc3af176f4da899dd487b55fe98664
Remi Marchand <remievan@hotmail.com>
2021-01-21 21:30:20 -0500

    Add prettierignore and run on more files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-    theme: {
-      textIndent: (theme, { negative }) => ({
-        ...{
-          sm: '2rem',
-          md: '3rem',
-          lg: '4rem',
-        },
-        ...negative({
-          sm: '2rem',
-          md: '3rem',
-          lg: '4rem',
-        }),
+  theme: {
+    textIndent: (theme, { negative }) => ({
+      ...{
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
+      },
+      ...negative({
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
       }),
-    } ,
-    variants: {},
-    plugins: [
-      require("tailwindcss-text-indent")()
-    ],
-  }
+    }),
+  },
+  variants: {},
+  plugins: [require("tailwindcss-text-indent")()],
+};
