54ad9c6e1ab8f8bbf849bd9bedc1f961d53a96a4
Remi Marchand <remievan@hotmail.com>
2021-01-21 23:13:40 -0500

    Add global annotations to tailwind style

--- src/routes/tailwind.svelte
+++ src/routes/tailwind.svelte
@@ -1,57 +1,55 @@
 <style global>
+  @tailwind base;
 
-@tailwind base;
+  :global(h1) {
+    @apply text-3xl;
+  }
 
-h1 {
-  @apply text-3xl;
-}
+  :global(h2) {
+    @apply text-2xl;
+    @apply mt-4;
+    @apply mb-4;
+  }
 
-h2 {
-  @apply text-2xl;
-  @apply mt-4;
-  @apply mb-4;
-}
+  :global(h3) {
+    @apply text-xl;
+    @apply mt-4;
+    @apply mb-4;
+  }
 
-h3 {
-  @apply text-xl;
-  @apply mt-4;
-  @apply mb-4;
-}
+  :global(p) {
+    @apply mt-6;
+    @apply mb-6;
+    line-height: 1.5;
+  }
 
-p {
-  @apply mt-6;
-  @apply mb-6;
-  line-height: 1.5;
-}
+  :global(ul) {
+    list-style: circle inside;
+    @apply ml-6;
+  }
 
-ul {
-  list-style: circle inside;
-  @apply ml-6;
-}
+  :global(a) {
+    @apply text-blue-500;
+  }
 
-a {
-  @apply text-blue-500;
-}
+  :global(pre) {
+    @apply p-2;
+    @apply bg-gray-200;
+    white-space: pre-wrap;
+    word-break: break-all;
+  }
 
-pre {
-  @apply p-2;
-  @apply bg-gray-200;
-  white-space: pre-wrap;
-  word-break: break-all;
-}
+  :global(code) {
+    @apply bg-gray-200;
+  }
 
-code {
-  @apply bg-gray-200;
-}
+  :global(html,
+  body,
+  main,
+  #sapper) {
+    height: 100%;
+  }
 
-html,
-body,
-main,
-#sapper {
-  height: 100%;
-}
-
-@tailwind components;
-@tailwind utilities;
-
+  @tailwind components;
+  @tailwind utilities;
 </style>
